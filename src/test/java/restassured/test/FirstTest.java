0dbf21238abfac896d332d6d83d5f6dea28f6aa7
ber3 <berenika.mazan@gmail.com>
2023-12-29 15:27:50 +0100

    change URL

--- src/test/java/restassured/test/FirstTest.java
+++ src/test/java/restassured/test/FirstTest.java
@@ -7,6 +7,6 @@ import static io.restassured.RestAssured.given;
 public class FirstTest {
     @Test
     public void givenNonExistingPetIdWhenGetPetThenPetNotFoundTest() {
-        given().when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/0").then().statusCode(404);
+        given().when().get("https://petstore.swagger.io/v2/pet/0").then().statusCode(404);
     }
 }
