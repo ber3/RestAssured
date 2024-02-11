74178a4e846b6f0c0e929f6f0664ce60f7f46e98
ber3 <berenika.mazan@gmail.com>
2024-02-11 18:09:18 +0100

    generate default getter/ setter with lombock

--- src/main/java/pl/bamnwa/objects/pet/Pet.java
+++ src/main/java/pl/bamnwa/objects/pet/Pet.java
@@ -1,61 +1,16 @@
 package pl.bamnwa.objects.pet;
 
-import java.util.ArrayList;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
-
+@Getter
+@Setter
 public class Pet {
-    public Long id;
-    public Category category;
-    public String name;
-    public List<String> photoUrls;
-    public List<Tag> tags;
-    public String status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<String> getPhotoUrls() {
-        return photoUrls;
-    }
-
-    public void setPhotoUrls(List<String> photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    public List<Tag> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<Tag> tags) {
-        this.tags = tags;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    private Long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+    private String status;
 }
--- src/main/java/pl/bamnwa/objects/pet/Tag.java
+++ src/main/java/pl/bamnwa/objects/pet/Tag.java
@@ -1,22 +1,12 @@
 package pl.bamnwa.objects.pet;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Tag {
-    public Long id;
-    public String name;
+    private Long id;
+    private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
